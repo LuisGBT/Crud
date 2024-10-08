575c34f1721998818b8850a60cd57d7ecc819071
LuisGTB <luizingustavo507@gmail.com>
2024-10-07 22:03:26 -0300

    feat

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const mysql = require('mysql2');
 // Add bootstrap
 app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
 
+// Manipulação dos dados via rota
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 // Import model express-handlebars
-const { engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 //Config  handleabrs
 app.engine('handlebars', engine());
@@ -36,9 +40,94 @@ conection.connect(function(error){
   }
 })
 
-// Route main
+// Route form
 app.get('/form', function(req, res) {
   res.render('form');
+})
+
+// Route list
+app.get('/list', function (req, res) {
+  // Sql query
+  let sql = 'SELECT * FROM client';
+
+  // Execute query
+  conection.query(sql, function (error, ret) {
+    if (error) throw (error);
+
+    //Object client receives return from query
+    res.render('list', {client:ret});
+  });
+})
+
+// Route register
+app.post('/register', function(req, res) {
+
+  // Geting data form
+  var name = req.body.name;
+  var age = req.body.age;
+  var email = req.body.email;
+  var password = req.body.password;
+  
+  // Sql query
+  let sql = `INSERT INTO client (name, idade, email, password) 
+  VALUES ('${name}', ${age}, '${email}', '${password}')`;
+
+  // Execute query
+  conection.query(sql, function(error, ret) {
+      if(error) throw error;
+      console.log(ret);
+      
+  });
+  // Redirect to from
+  res.redirect('/list');
+  res.end();
+})
+
+// Route delete
+app.get('/delet/:id', function (req, res) {
+  // Sql query
+  let sql = `DELETE FROM client WHERE id = ${req.params.id}`;
+  // Execute query
+  conection.query(sql, function(error, ret) {
+    if(error) throw (error);
+    console.log(ret);
+  })
+  
+   // Redirect to from
+  res.redirect('/form');
+  res.end();
+})
+
+//Route edit
+app.get('/edit/:id', function(req, res) {
+  let sql = `SELECT * FROM client WHERE id= ${req.params.id}`;
+
+  conection.query(sql, function(error, ret) {
+    if(error) throw (error);
+    console.log(ret);
+    res.render('edit', {client:ret});
+  })
+  
+})
+
+//Route Update
+app.post('/update', function(req, res) {
+  var id = req.body.id;
+  var name = req.body.name;
+  var age = req.body.age;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  let sql = `UPTADE FROM client (name, idade, email, password) VALUES
+  ('${name}', ${age}, '${email}', '${password}') WHERE id = ${id}`;
+
+  conection.query(sql, function(error, ret) {
+    if(error) throw (error);
+    console.log(ret);
+  })
+  res.redirect('/list');
+  res.end();
+
 })
 
 //Server
